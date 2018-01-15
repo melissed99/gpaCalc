549ce90b2e31761227963075d62e324658b26082
Melisse Doroteo <gabbie_doroteo@yahoo.com>
2018-01-14 22:42:26 -0700

    Initial Commit

---
+++ gpaCalc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gpaCalc
+//
+//  Created by Gabbie Doroteo on 2018-01-14.
+//  Copyright © 2018 Gabbie Doroteo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
